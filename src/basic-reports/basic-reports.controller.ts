1b5debf747b3659222bbacaf970ed17ea7a45444
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-08-12 18:05:49 -0300

    refactor code

--- src/basic-reports/basic-reports.controller.ts
+++ src/basic-reports/basic-reports.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { BasicReportsService } from './basic-reports.service';
+import { Response } from 'express';
 
 @Controller('basic-reports')
 export class BasicReportsController {
   constructor(private readonly basicReportsService: BasicReportsService) {}
   @Get()
-  hello() {
-    return this.basicReportsService.hello();
+  hello(@Res() res: Response) {
+    const pdfDoc = this.basicReportsService.hello();
+    res.setHeader('Content-Type', 'application/pdf');
+    pdfDoc.info.Title = 'Hello World';
+    pdfDoc.pipe(res);
+    pdfDoc.end();
   }
 }
--- src/basic-reports/basic-reports.service.ts
+++ src/basic-reports/basic-reports.service.ts
@@ -1,15 +1,22 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrinterService } from 'src/printer/printer.service';
+import { getHelloWorldReport } from 'src/reports/hello-world.report';
 
 @Injectable()
 export class BasicReportsService extends PrismaClient implements OnModuleInit {
   async onModuleInit(): Promise<void> {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     await this.$connect();
     console.log('Connected to database');
   }
 
+  constructor(private readonly printerService: PrinterService) {
+    super();
+  }
+
   hello() {
-    return this.employees.findFirst();
+    const docDefinition = getHelloWorldReport();
+    const doc = this.printerService.createPdf(docDefinition);
+    return doc;
   }
 }
