1b47213c67577e90af421663ed665ee0421f80e3
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-08-12 14:53:51 -0300

    connect database

--- src/basic-reports/basic-reports.controller.ts
+++ src/basic-reports/basic-reports.controller.ts
@@ -1,7 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BasicReportsService } from './basic-reports.service';
 
 @Controller('basic-reports')
 export class BasicReportsController {
   constructor(private readonly basicReportsService: BasicReportsService) {}
+  @Get()
+  hello() {
+    return this.basicReportsService.hello();
+  }
 }
--- src/basic-reports/basic-reports.service.ts
+++ src/basic-reports/basic-reports.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class BasicReportsService {}
+export class BasicReportsService extends PrismaClient implements OnModuleInit {
+  async onModuleInit(): Promise<void> {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    await this.$connect();
+    console.log('Connected to database');
+  }
+
+  hello(): string {
+    return 'Hello World';
+  }
+}
