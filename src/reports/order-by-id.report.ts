7532fa0244d7bad21be314a7768143bd04c16746
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-08-19 12:44:15 -0300

    adding products table

--- src/reports/order-by-id.report.ts
+++ src/reports/order-by-id.report.ts
@@ -6,6 +6,7 @@ import {
 import { cmToPoints } from 'src/helpers/cmToPoint.helper';
 import { DateFormatter } from 'src/helpers/date-formatter.helper';
 import { footerSection } from './sections/footer.section';
+import { CurrencyFormatter } from 'src/helpers/currency-formatter';
 
 const styles: StyleDictionary = {
   header: {
@@ -62,6 +63,69 @@ export const orderByIdReport = (): TDocumentDefinitions => {
            CUIT: 20-12312312-1
            Domicilio: calle falsa 123, Puerto Vilelas
            Chaco, ARGENTINA.`,
+        ],
+        marginBottom: cmToPoints(1),
+      },
+      {
+        layout: 'headerLineOnly',
+        table: {
+          headerRows: 1,
+          widths: [50, '*', 'auto', 'auto', 'auto'],
+          body: [
+            ['ID', 'Descripción', 'Cantidad', 'Precio', 'Subtotal'],
+            [
+              '1',
+              'Descripción',
+              '1',
+              '1',
+              {
+                text: CurrencyFormatter.formatCurrency(100),
+                alignment: 'right',
+              },
+            ],
+            [
+              '2',
+              'Descripción',
+              '1',
+              '1',
+              {
+                text: CurrencyFormatter.formatCurrency(1400),
+                alignment: 'right',
+              },
+            ],
+          ],
+        },
+      },
+      {
+        margin: [0, cmToPoints(1)],
+        columns: [
+          {
+            text: '',
+            width: '*',
+          },
+          {
+            width: 'auto',
+            layout: 'noBorders',
+            table: {
+              body: [
+                [
+                  'Subtotal',
+                  {
+                    text: CurrencyFormatter.formatCurrency(1500),
+                    alignment: 'right',
+                  },
+                ],
+                [
+                  { text: 'Total', bold: true },
+                  {
+                    text: CurrencyFormatter.formatCurrency(1500),
+                    alignment: 'right',
+                    bold: true,
+                  },
+                ],
+              ],
+            },
+          },
         ],
       },
     ],
